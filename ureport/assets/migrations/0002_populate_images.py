848609ce92c24a994b218cef784f5ab3cdc3eb61
Norbert Kwizera <norkans7@gmail.com>
2015-04-28 14:30:30 +0200

    Add s3 support for populate image migrations

--- ureport/assets/migrations/0002_populate_images.py
+++ ureport/assets/migrations/0002_populate_images.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import urllib
 
 from django.db import models, migrations
 from django.core.files.images import ImageFile
@@ -10,21 +11,28 @@ def populate_images(apps, schema_editor):
     Image = apps.get_model('assets', "Image")
 
     for org_bg in OrgBackground.objects.all():
+        image_file = None
         try:
             image_file = open(org_bg.image.path, 'rb')
-            django_image_file = ImageFile(image_file)
-            image_filename = org_bg.image.path.split('/')[-1]
-
-            image_obj = Image()
-            image_obj.org = org_bg.org
-            image_obj.name = org_bg.name
-            image_obj.image_type = org_bg.background_type
-            image_obj.is_active = org_bg.is_active
-            image_obj.created_by = org_bg.created_by
-            image_obj.modified_by = org_bg.modified_by
-            image_obj.image.save(image_filename, django_image_file, save=True)
+        except NotImplementedError:
+            retrived_image = urllib.urlretrieve(org_bg.image.url)
+            image_file = open(retrived_image[0])
         except IOError:
             pass
+
+        finally:
+            if image_file:
+                django_image_file = ImageFile(image_file)
+                image_filename = org_bg.image.path.split('/')[-1]
+
+                image_obj = Image()
+                image_obj.org = org_bg.org
+                image_obj.name = org_bg.name
+                image_obj.image_type = org_bg.background_type
+                image_obj.is_active = org_bg.is_active
+                image_obj.created_by = org_bg.created_by
+                image_obj.modified_by = org_bg.modified_by
+                image_obj.image.save(image_filename, django_image_file, save=True)
 
 
 class Migration(migrations.Migration):
