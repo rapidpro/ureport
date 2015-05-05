4ed905d248991592578501b01fd4228e92229b35
Norbert Kwizera <norkans7@gmail.com>
2015-05-05 17:18:32 +0200

    Add image types constants

--- ureport/utils/__init__.py
+++ ureport/utils/__init__.py
@@ -1,6 +1,6 @@
 from dash.orgs.models import Org
 from django.conf import settings
-from ureport.assets.models import Image
+from ureport.assets.models import Image, FLAG
 
 
 def get_linked_orgs():
@@ -13,7 +13,7 @@ def get_linked_orgs():
         host = settings.SITE_HOST_PATTERN % org.subdomain
         org.host = host
         if org.get_config('is_on_landing_page'):
-            flag = Image.objects.filter(org=org, is_active=True, image_type='F').first()
+            flag = Image.objects.filter(org=org, is_active=True, image_type=FLAG).first()
             if flag:
                 linked_sites.append(dict(name=org.name, host=host, flag=flag.image.url, is_static=False))
 
