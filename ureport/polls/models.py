b141d015a002792d511440d68664bac5664fb355
Norbert Kwizera <norkans7@gmail.com>
2015-05-08 09:24:50 +0200

    make sure the main poll is shown in bricks poll for global site

--- ureport/polls/models.py
+++ ureport/polls/models.py
@@ -75,6 +75,11 @@ class Poll(SmartModel):
                 polls = polls.exclude(pk=main_poll.pk)
 
             brick_polls = []
+
+            # for the global site the main poll should be the first in the brick polls
+            if org.get_config('is_global'):
+                brick_polls.append(main_poll)
+
             for poll in polls:
                 if poll.get_first_question:
                     brick_polls.append(poll)
