6787850ca99b657d3bd8f1b1bb3730a1538719ed
Norbert Kwizera <norkans7@gmail.com>
2015-07-30 16:25:15 +0200

    hide login link for custom domain and no index for their subdomain

--- ureport/public/context_processors.py
+++ ureport/public/context_processors.py
@@ -1,4 +1,16 @@
 
+def set_dot_in_hidden(request):
+    dot_in_hidden = True
+    if request.META.get('HTTP_HOST', '').find('ureport.in') >= 0 and not request.org.domain:
+        dot_in_hidden = False
+
+    login_hidden = False
+    if request.org.domain:
+        login_hidden = True
+
+    return dict(dot_in_hidden=dot_in_hidden, login_hidden=login_hidden)
+
+
 def set_is_iorg(request):
     """
     Context Processor that populates the 'is_internet_org' context variable with whether
