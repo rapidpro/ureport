6640c8232f185328f01968208ffe7c92097beba2
Norbert Kwizera <norkans7@gmail.com>
2016-01-14 21:58:49 +0200

    cleanup unused code

--- ureport/utils/__init__.py
+++ ureport/utils/__init__.py
@@ -105,82 +105,6 @@ def clean_global_results_data(org, results, segment):
     return results
 
 
-def fetch_contact_field_results(org, contact_field, segment):
-    from ureport.polls.models import CACHE_ORG_FIELD_DATA_KEY, UREPORT_ASYNC_FETCHED_DATA_CACHE_TIME
-    from ureport.polls.models import UREPORT_RUN_FETCHED_DATA_CACHE_TIME
-
-    start = time.time()
-    print "Fetching  %s for %s with segment %s" % (contact_field, org.name, segment)
-
-    cache_time = UREPORT_ASYNC_FETCHED_DATA_CACHE_TIME
-    if segment and segment.get('location', "") == "District":
-        cache_time = UREPORT_RUN_FETCHED_DATA_CACHE_TIME
-
-    try:
-        segment = substitute_segment(org, segment)
-
-        this_time = datetime.now()
-
-        temba_client = org.get_temba_client()
-        client_results = temba_client.get_results(contact_field=contact_field, segment=segment)
-
-        results_data = temba_client_flow_results_serializer(client_results)
-        cleaned_results_data = results_data
-
-        print "Fetch took %ss" % (time.time() - start)
-
-        key = CACHE_ORG_FIELD_DATA_KEY % (org.pk, slugify(unicode(contact_field)), slugify(unicode(segment)))
-        cache.set(key, {'time': datetime_to_ms(this_time), 'results': cleaned_results_data}, cache_time)
-    except:
-        client.captureException()
-        import traceback
-        traceback.print_exc()
-
-
-def get_contact_field_results(org, contact_field, segment):
-    from ureport.polls.models import CACHE_ORG_FIELD_DATA_KEY
-
-    subsituted_segment = substitute_segment(org, segment)
-
-    key = CACHE_ORG_FIELD_DATA_KEY % (org.pk, slugify(unicode(contact_field)), slugify(unicode(subsituted_segment)))
-    cache_value = cache.get(key, None)
-
-    if cache_value:
-        return cache_value['results']
-
-    if segment and segment.get('location', "") == "District":
-        fetch_contact_field_results(org, contact_field, segment)
-        cache_value = cache.get(key, None)
-        if cache_value:
-            return cache_value['results']
-
-
-def get_most_active_regions(org):
-    cache_key = 'most_active_regions:%d' % org.id
-    active_regions = cache.get(cache_key, None)
-
-    if active_regions is None:
-        regions = org.get_contact_field_results(org.get_config('gender_label'), dict(location="State"))
-        active_regions = dict()
-
-        if not regions:
-            return []
-
-        for region in regions:
-            active_regions[region['label']] = region['set'] + region['unset']
-
-        tuples = [(k, v) for k, v in active_regions.iteritems()]
-        tuples.sort(key=lambda t: t[1], reverse=True)
-
-        active_regions = [k for k, v in tuples]
-        cache.set(cache_key, active_regions, 3600 * 24)
-
-    if org.get_config('is_global'):
-        active_regions = [pycountry.countries.get(alpha2=elt).name for elt in active_regions]
-
-    return active_regions
-
-
 def organize_categories_data(org, contact_field, api_data):
 
     cleaned_categories = []
@@ -550,8 +474,6 @@ Org.get_ureporters_locations_stats = get_ureporters_locations_stats
 Org.get_registration_stats = get_registration_stats
 Org.get_age_stats = get_age_stats
 Org.get_gender_stats = get_gender_stats
-Org.get_contact_field_results = get_contact_field_results
-Org.get_most_active_regions = get_most_active_regions
 Org.get_regions_stats = get_regions_stats
 Org.organize_categories_data = organize_categories_data
 Org.get_flows = get_flows
--- ureport/utils/tests.py
+++ ureport/utils/tests.py
@@ -173,39 +173,6 @@ class UtilsTest(DashTest):
         self.assertFalse('parent' in json.loads(global_segment))
         self.assertEqual(global_segment, json.dumps(expected))
 
-    def test_get_most_active_regions(self):
-        self.org.set_config('gender_label', 'Gender')
-        self.org.set_config("state_label", "Province")
-
-        with patch('dash.orgs.models.Org.get_contact_field_results') as mock:
-            mock.return_value = [dict(label='LABEL_1', set=15, unset=5, categories=[], open_ended=None),
-                                 dict(label='LABEL_2', set=100, unset=200, categories=[], open_ended=None),
-                                 dict(label='LABEL_3', set=50, unset=30, categories=[], open_ended=None)]
-
-            self.assertEquals(self.org.get_most_active_regions(), ['LABEL_2', 'LABEL_3', 'LABEL_1'])
-            mock.assert_called_once_with('Gender', dict(location='State'))
-
-        with patch('dash.orgs.models.Org.get_contact_field_results') as mock:
-            self.clear_cache()
-            mock.return_value = None
-
-            self.assertEquals(self.org.get_most_active_regions(), [])
-            mock.assert_called_once_with('Gender', dict(location='State'))
-
-        self.org.set_config("is_global", True)
-
-        with patch('dash.orgs.models.Org.get_contact_field_results') as mock:
-            mock.return_value = [dict(label='UG', set=15, unset=5, categories=[], open_ended=None),
-                                 dict(label='RW', set=100, unset=200, categories=[], open_ended=None),
-                                 dict(label='US', set=50, unset=30, categories=[], open_ended=None)]
-
-            self.assertEquals(self.org.get_most_active_regions(), ['Rwanda', 'United States', 'Uganda'])
-            segment = dict()
-            segment["contact_field"] = "Province"
-            segment["values"] = [elt.alpha2 for elt in pycountry.countries.objects]
-
-            mock.assert_called_once_with('Gender', dict(location='State'))
-
     def test_organize_categories_data(self):
 
         self.org.set_config('born_label', "Born")
