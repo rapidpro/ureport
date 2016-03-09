865f52e5a496fc7d9552c02ab294b71def5ecdd2
Norbert Kwizera <norkans7@gmail.com>
2016-03-09 11:47:23 +0200

    add logs in pull contacts tasks

--- ureport/contacts/tasks.py
+++ ureport/contacts/tasks.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import time
 from dash.orgs.tasks import org_task
 from celery.utils.log import get_task_logger
 
@@ -17,11 +18,33 @@ def pull_contacts(org, since, until):
     if not since:
         logger.warn("First time run for org #%d. Will sync all contacts" % org.pk)
 
+    start = time.time()
+
     fields_created, fields_updated, fields_deleted, ignored = backend.pull_fields(org)
+
+    logger.warn("Fetched contact fields for org #%d. "
+                "Created %s, Updated %s, Deleted %d, Ignored %d" % (org.pk, fields_created, fields_updated,
+                                                                    fields_deleted, ignored))
+    logger.warn("Fetch fields for org #%d (%s) took %ss" % (org.pk, str(org.name), time.time() - start))
+
+    start_boundaries = time.time()
 
     boundaries_created, boundaries_updated, boundaries_deleted, ignored = backend.pull_boundaries(org)
 
+    logger.warn("Fetched contact fields for org #%d. "
+                "Created %s, Updated %s, Deleted %d, Ignored %d" % (org.pk, boundaries_created, boundaries_updated,
+                                                                    boundaries_deleted, ignored))
+
+    logger.warn("Fetch boundaries for org #%d (%s) took %ss" % (org.pk, str(org.name), time.time() - start_boundaries))
+    start_contacts = time.time()
+
     contacts_created, contacts_updated, contacts_deleted, ignored = backend.pull_contacts(org, since, until)
+
+    logger.warn("Fetched contacts for org #%d. "
+                "Created %s, Updated %s, Deleted %d, Ignored %d" % (org.pk, contacts_created, contacts_updated,
+                                                                    contacts_deleted, ignored))
+
+    logger.warn("Fetch contacts for org #%d (%s) took %ss" % (org.pk, str(org.name), time.time() - start_contacts))
 
     return {
         'fields': {'created': fields_created, 'updated': fields_updated, 'deleted': fields_deleted},
