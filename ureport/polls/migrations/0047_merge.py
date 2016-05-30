974a20c65a190f4f68138fa384dd7b3d218ac062
teehamaral <contato@teehamaral.me>
2016-05-30 23:31:35 +0000

    Add new migrations

---
+++ ureport/polls/migrations/0047_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0040_merge'),
+        ('polls', '0046_add_index_on_pollresults_contact'),
+    ]
+
+    operations = [
+    ]
