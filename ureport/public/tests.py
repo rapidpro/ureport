eca7312f62e40a3fef38eb8683913760c9e601ad
Teeh Amaral <contato@teehamaral.me>
2016-07-27 20:16:00 -0300

    Change number org config fields

--- ureport/public/tests.py
+++ ureport/public/tests.py
@@ -49,12 +49,12 @@ class PublicTest(DashTest):
         self.login(self.admin)
         response = self.client.get(edit_url, SERVER_NAME='nigeria.ureport.io')
         self.assertTrue('form' in response.context)
-        self.assertEquals(len(response.context['form'].fields), 17)
+        self.assertEquals(len(response.context['form'].fields), 16)
 
         self.login(self.superuser)
         response = self.client.get(edit_url, SERVER_NAME='nigeria.ureport.io')
         self.assertTrue('form' in response.context)
-        self.assertEquals(len(response.context['form'].fields), 35)
+        self.assertEquals(len(response.context['form'].fields), 34)
 
     def test_chooser(self):
         chooser_url = reverse('public.home')
