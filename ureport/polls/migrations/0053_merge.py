3bcc2dfc1c9a3a71df9363d5f099a48d23799fdb
teehamaral <contato@teehamaral.me>
2016-08-11 14:02:53 +0000

    Add merge migrations

---
+++ ureport/polls/migrations/0053_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0049_auto_20160810_1823'),
+        ('polls', '0052_merge'),
+    ]
+
+    operations = [
+    ]
