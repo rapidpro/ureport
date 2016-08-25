e4c77343c3bd49dd4fc6c69e3e16a7eab111e7e7
Norbert Kwizera <norkans7@gmail.com>
2016-08-25 18:16:42 +0200

    log the cursor we saved to redis

--- ureport/backend/rapidpro.py
+++ ureport/backend/rapidpro.py
@@ -446,12 +446,15 @@ class RapidProBackend(BaseBackend):
 
                     if num_synced >= Poll.POLL_RESULTS_MAX_SYNC_RUNS:
                         poll.rebuild_poll_results_counts()
+                        cursor = fetches.get_cursor()
+
                         cache.set(Poll.POLL_RESULTS_LAST_PULL_CURSOR % (org.pk, poll.flow_uuid),
-                                  fetches.get_cursor(), None)
+                                  cursor, None)
 
                         print "Break pull results for poll #%d on org #%d in %ds, " \
-                              "created %d, updated %d, ignored %d" % (poll.pk, org.pk, time.time() - start, num_created,
-                                                                      num_updated, num_ignored)
+                              "created %d, updated %d, ignored %. Before cursor %s" % (poll.pk, org.pk,
+                                                                                       time.time() - start, num_created,
+                                                                                       num_updated, num_ignored, cursor)
 
                         return num_created, num_updated, num_ignored
 
