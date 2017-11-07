90d8fe93e3c5e6e75e41a09b874909ecbe66aee2
Norbert Kwizera <norkans7@gmail.com>
2017-11-07 09:29:41 +0200

    Add Vietnamese

--- ureport/settings_common.py
+++ ureport/settings_common.py
@@ -59,9 +59,19 @@ MODELTRANSLATION_TRANSLATION_REGISTRY = "translation"
 LANGUAGE_CODE = 'en'
 
 # Available languages for translation
-LANGUAGES = (('en', "English"), ('fr', "French"), ('es', "Spanish"), ('ar', "Arabic"), ('pt', "Portuguese"),
-             ('pt-br', "Brazilian Portuguese"), ('uk', "Ukrainian"), ('my', "Burmese"), ('id', "Indonesian"),
-             ('it', "Italian"), ('ro', "Romanian"))
+LANGUAGES = (('en', "English"),
+             ('fr', "French"),
+             ('es', "Spanish"),
+             ('ar', "Arabic"),
+             ('pt', "Portuguese"),
+             ('pt-br', "Brazilian Portuguese"),
+             ('uk', "Ukrainian"),
+             ('my', "Burmese"),
+             ('id', "Indonesian"),
+             ('it', "Italian"),
+             ('ro', "Romanian"),
+             ('vi', 'Vietnamese'))
+
 DEFAULT_LANGUAGE = "en"
 RTL_LANGUAGES = ['ar']
 
@@ -75,7 +85,8 @@ ORG_LANG_MAP = {
     'my': 'my_MM',
     'pt': 'pt_PT',
     'pt-br': 'pt_BR',
-    'uk': 'uk_UA'
+    'uk': 'uk_UA',
+    'vi': 'vi_VN'
 }
 
 
