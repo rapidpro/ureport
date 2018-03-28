d99dd03d67bc2212be1406cf210420b9b048cc33
Norbert Kwizera <norkans7@gmail.com>
2018-03-28 16:07:01 +0200

    Remove unused import

--- ureport/backend/rapidpro.py
+++ ureport/backend/rapidpro.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
-from future.utils import listvalues
 
 from collections import defaultdict
 import json
