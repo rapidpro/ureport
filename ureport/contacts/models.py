55f3ff997537804ede998052d7724e6ff03d7779
Norbert Kwizera <norkans7@gmail.com>
2018-04-06 16:01:47 +0200

    Use cache to store the time we last fetched contact by backend

--- ureport/contacts/models.py
+++ ureport/contacts/models.py
@@ -58,7 +58,7 @@ class Contact(models.Model):
     Corresponds to a RapidPro contact
     """
 
-    CONTACT_LAST_FETCHED_CACHE_KEY = 'last:fetch_contacts:%d'
+    CONTACT_LAST_FETCHED_CACHE_KEY = 'last:fetch_contacts:%d:backend:%s'
     CONTACT_LAST_FETCHED_CACHE_TIMEOUT = 60 * 60 * 24 * 30
 
     MALE = 'M'
