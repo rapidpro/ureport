8ffc6f39bca8f2e4a843663b99178114d198a8c2
John Cordeiro <john.dalton@ilhasoft.com.br>
2018-05-18 08:42:23 -0300

    Add western balkans on the bottom of the page

--- ureport/settings_common.py
+++ ureport/settings_common.py
@@ -653,6 +653,13 @@ PREVIOUS_ORG_SITES = [
         count_link="http://uk.ureport.in/count/",
     ),
     dict(
+        name='Western Balkans',
+        host="//westernbalkans.ureport.in",
+        flag="flag_wb.png",
+        is_static=True,
+        count_link="http://westernbalkans.ureport.in/count/",
+    ),
+    dict(
         name="Zambia",
         host="//www.zambiaureport.com/home/",
         flag="flag_zm.png",
