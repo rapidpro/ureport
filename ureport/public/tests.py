6274b592a99c016a633c8e6f612547e9589ad510
John Cordeiro <john.dalton@ilhasoft.com.br>
2018-05-18 08:49:50 -0300

    Fix org config fields tests by incrementing the new field created

--- ureport/public/tests.py
+++ ureport/public/tests.py
@@ -55,7 +55,7 @@ class PublicTest(UreportTest):
         self.login(self.admin)
         response = self.client.get(edit_url, SERVER_NAME='nigeria.ureport.io')
         self.assertTrue('form' in response.context)
-        self.assertEqual(len(response.context['form'].fields), 27)
+        self.assertEqual(len(response.context['form'].fields), 28)
 
         self.login(self.superuser)
         response = self.client.get(edit_url, SERVER_NAME='nigeria.ureport.io')
