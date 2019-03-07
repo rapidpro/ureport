70d85f817cab1b03b8742bd297c79492856a56d9
Eduardo Douglas <edudouglas@gmail.com>
2019-03-07 15:29:23 -0300

    Remove leafet background, updated docker-compose and settings

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -11,4 +11,4 @@ echo "Compile Messages"
 python manage.py compilemessages
 
 echo "Starting server"
-gunicorn ureport.wsgi:application -t 120 -w 4 --max-requests 5000 -b 0.0.0.0:8080
+gunicorn ureport.wsgi:application -t 120 -w 2 --max-requests 5000 -b 0.0.0.0:8080
