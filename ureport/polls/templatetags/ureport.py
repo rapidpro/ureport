a341b4d01019ed9fdd7482209d33f2944b68816a
Norbert Kwizera <norkans7@gmail.com>
2019-04-15 15:19:05 +0200

    Add fields styles for Bulma CSS

--- ureport/polls/templatetags/ureport.py
+++ ureport/polls/templatetags/ureport.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import logging
 
-from django import template
+from django import forms, template
 from django.conf import settings
 from django.template import TemplateSyntaxError
 from django.utils.safestring import mark_safe
@@ -161,3 +161,45 @@ def org_host_link(context):
         return org.build_host_link(True)
     except Exception:
         return "https://%s" % getattr(settings, "HOSTNAME", "localhost")
+
+
+@register.filter
+def is_select(field):
+    return isinstance(field.field.widget, forms.Select)
+
+
+@register.filter
+def is_multiple_select(field):
+    return isinstance(field.field.widget, forms.SelectMultiple)
+
+
+@register.filter
+def is_textarea(field):
+    return isinstance(field.field.widget, forms.Textarea)
+
+
+@register.filter
+def is_input(field):
+    return isinstance(
+        field.field.widget, (forms.TextInput, forms.NumberInput, forms.EmailInput, forms.PasswordInput, forms.URLInput)
+    )
+
+
+@register.filter
+def is_checkbox(field):
+    return isinstance(field.field.widget, forms.CheckboxInput)
+
+
+@register.filter
+def is_multiple_checkbox(field):
+    return isinstance(field.field.widget, forms.CheckboxSelectMultiple)
+
+
+@register.filter
+def is_radio(field):
+    return isinstance(field.field.widget, forms.RadioSelect)
+
+
+@register.filter
+def is_file(field):
+    return isinstance(field.field.widget, forms.FileInput)
