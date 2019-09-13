13edc268e5814b3567f12a17b04b5a6df4370201
Norbert Kwizera <norkans7@gmail.com>
2019-09-13 15:49:57 +0200

    Add config value for homepage video

--- ureport/settings_common.py
+++ ureport/settings_common.py
@@ -246,7 +246,14 @@ ORG_CONFIG_FIELDS = [
             help_text=_("The short text used to direct visitors to join U-Report"), label="Join Text", required=False
         ),
     ),
-
+    dict(
+        name="homepage_join_video_id",
+        field=dict(
+            help_text=_("The YouTube video ID for how to join U-Report section"),
+            label="Homepage Video ID",
+            required=False,
+        ),
+    ),
     dict(
         name="dark1_color",
         field=dict(help_text=_("The primary color for styling for this organization, should be dark"), required=False),
@@ -254,70 +261,69 @@ ORG_CONFIG_FIELDS = [
     ),
     dict(
         name="dark2_color",
-        field=dict(help_text=_("The secondary color for styling for this organization, should be dark"),
-                   required=False),
+        field=dict(
+            help_text=_("The secondary color for styling for this organization, should be dark"), required=False
+        ),
         superuser_only=True,
     ),
     dict(
         name="dark3_color",
-        field=dict(help_text=_("The tertiary color for styling for this organization, should be dark"), required=False),
+        field=dict(
+            help_text=_("The tertiary color for styling for this organization, should be dark"), required=False
+        ),
         superuser_only=True,
     ),
-
     dict(
         name="light1_color",
-        field=dict(help_text=_("The primary highlight color for styling for this organization, should be light"),
-                   required=False),
+        field=dict(
+            help_text=_("The primary highlight color for styling for this organization, should be light"),
+            required=False,
+        ),
         superuser_only=True,
     ),
     dict(
         name="light2_color",
-        field=dict(help_text=_("The secondary highlight color for styling for this organization, should be light"),
-                   required=False),
+        field=dict(
+            help_text=_("The secondary highlight color for styling for this organization, should be light"),
+            required=False,
+        ),
         superuser_only=True,
     ),
-
     dict(
         name="colors",
         field=dict(help_text=_("Up to 6 colors for styling charts, use comma between colors"), required=False),
         superuser_only=True,
     ),
-
     # deprecated, can be removed after v2 launch
     dict(
         name="join_fg_color",
         field=dict(help_text=_("The color used to draw the text on the join bar"), required=False),
         superuser_only=True,
     ),
-
     # deprecated, can be removed after v2 launch
     dict(
         name="join_bg_color",
         field=dict(help_text=_("The color used to draw the background on the join bar"), required=False),
         superuser_only=True,
     ),
-
     # deprecated, should be replaced by dark1
     dict(
         name="primary_color",
         field=dict(help_text=_("The primary color for styling for this organization"), required=False),
         superuser_only=True,
     ),
-
     # deprecated, should be replaced by dark2
     dict(
         name="secondary_color",
         field=dict(help_text=_("The secondary color for styling for this organization"), required=False),
         superuser_only=True,
     ),
-
     # deprecated, can be removed after v2 launch
     dict(
         name="bg_color",
         field=dict(help_text=_("The background color for the site"), required=False),
         superuser_only=True,
     ),
-
     dict(
         name="colors_map",
         field=dict(
