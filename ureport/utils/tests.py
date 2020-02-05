a0469085ff2a88da1e3901e16ca3ac8e2923b2a8
Norbert Kwizera <norkans7@gmail.com>
2020-02-05 17:52:59 +0200

    Update deps celery, redis and django-redis

--- ureport/utils/tests.py
+++ ureport/utils/tests.py
@@ -525,7 +525,7 @@ class UtilsTest(UreportTest):
             CACHES={
                 "default": {
                     "BACKEND": "django_redis.cache.RedisCache",
-                    "LOCATION": "127.0.0.1:6379:1",
+                    "LOCATION": "redis://127.0.0.1:6379/1",
                     "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
                 }
             }
