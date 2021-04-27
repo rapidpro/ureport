e85d7af730e4d5a6dc5db4323c74ed5ebfd22539
Norbert Kwizera <norkans7@gmail.com>
2021-04-27 19:48:56 +0200

    Run code checks

--- ureport/stats/migrations/0009_add_better_indexes.py
+++ ureport/stats/migrations/0009_add_better_indexes.py
@@ -2,7 +2,6 @@
 
 from django.db import migrations
 
-
 # language=SQL
 INDEX_SQL_CONTACTACTIVITY_ORG_DATE_GENDER_NOT_NULL = """
 CREATE INDEX IF NOT EXISTS stats_contactactivity_org_id_date_gender_not_null on stats_contactactivity (org_id, date, gender) WHERE gender IS NOT NULL;
@@ -17,7 +16,7 @@ CREATE INDEX IF NOT EXISTS stats_contactactivity_org_id_date_state_not_null on s
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('stats', '0008_add_index'),
+        ("stats", "0008_add_index"),
     ]
 
     operations = [
