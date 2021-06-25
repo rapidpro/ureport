153814720fbd67201e400dbd4ea8e6d25c7c62e1
Norbert Kwizera <norkans7@gmail.com>
2021-06-25 17:09:49 +0200

    Remove no longer used variables

--- ureport/polls/models.py
+++ ureport/polls/models.py
@@ -94,17 +94,9 @@ class Poll(SmartModel):
 
     POLL_RESULTS_MAX_SYNC_RUNS = 100_000
 
-    POLL_RESULTS_LAST_PULL_CURSOR = "last:poll_pull_results_cursor:org:%d:poll:%s"
-
     POLL_RESULTS_LAST_OTHER_POLLS_SYNCED_CACHE_KEY = "last:poll_last_other_polls_sync:org:%d:poll:%s"
 
     POLL_RESULTS_LAST_OTHER_POLLS_SYNCED_CACHE_TIMEOUT = 60 * 60 * 24 * 2
-
-    POLL_RESULTS_CURSOR_AFTER_CACHE_KEY = "last:poll_pull_results_cursor_after:org:%d:poll:%s"
-
-    POLL_RESULTS_CURSOR_BEFORE_CACHE_KEY = "last:poll_pull_results_cursor_before:org:%d:poll:%s"
-
-    POLL_RESULTS_BATCHES_LATEST_CACHE_KEY = "last:poll_pull_results_cursor_latest:org:%d:poll:%s"
 
     POLL_PULL_ALL_RESULTS_AFTER_DELETE_FLAG = "poll-results-pull-after-delete-flag:%s:%s"
 
