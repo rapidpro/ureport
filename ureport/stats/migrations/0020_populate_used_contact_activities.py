479b23625d8da9c3351c3e5a73d3d99c5bd77824
Norbert Kwizera <norkans7@gmail.com>
2021-10-18 16:36:00 +0200

    Make sure the contact activities of the last year get updated with used True

--- ureport/stats/migrations/0020_populate_used_contact_activities.py
+++ ureport/stats/migrations/0020_populate_used_contact_activities.py
@@ -3,21 +3,15 @@
 
 import logging
 import time
-
 from datetime import timedelta
-from django.db import migrations
 
 from django.core.cache import cache
-
+from django.db import migrations
+from django.utils import timezone
 
 from ureport.utils import chunk_list
 
-from django.utils import timezone
-
-
-
 logger = logging.getLogger(__name__)
-
 
 
 def noop(apps, schema_editor):  # pragma: no cover
@@ -42,16 +36,20 @@ def populate_contact_activities_used(apps, schema_editor):  # pragma: no cover
         contact_activities_used_max_id_key = f"contact_activities_used_max_id:{org.id}"
         max_id = cache.get(contact_activities_used_max_id_key, 0)
 
-        contact_activities_ids = ContactActivity.objects.filter(org=org, date__gte=start, id__gt=max_id).order_by("id").values_list("id", flat=True)
+        contact_activities_ids = (
+            ContactActivity.objects.filter(org=org, date__gte=start, id__gt=max_id)
+            .order_by("id")
+            .values_list("id", flat=True)
+        )
 
         org_count = 0
 
         for batch in chunk_list(contact_activities_ids, 1000):
             batch_ids = list(batch)
-            latest_id= batch_ids[-1]
-            
+            latest_id = batch_ids[-1]
+
             updated = ContactActivity.objects.filter(id__in=batch_ids).update(used=True)
-            
+
             org_count += updated
 
             elapsed = time.time() - start_time
@@ -68,12 +66,10 @@ def apply_manual():  # pragma: no cover
     populate_contact_activities_used(apps, None)
 
 
-
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('stats', '0019_contactactivity_used'),
+        ("stats", "0019_contactactivity_used"),
     ]
 
     operations = [migrations.RunPython(populate_contact_activities_used, noop)]
-
