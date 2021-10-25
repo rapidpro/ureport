366e076bc83a6ba488cf9c2e103850f2c6a73af0
Norbert Kwizera <norkans7@gmail.com>
2021-10-25 18:35:27 +0200

    Update deps

--- ureport/celery.py
+++ ureport/celery.py
@@ -15,10 +15,6 @@ app = Celery("ureport")
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object("django.conf:settings")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-
-@app.task(bind=True)
-def debug_task(self):
-    print("Request: {0!r}".format(self.request))
