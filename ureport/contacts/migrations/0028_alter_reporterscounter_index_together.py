ca2876f007d84b563462f18bac8c271da5a9cc3f
Norbert Kwizera <norkans7@gmail.com>
2021-11-04 15:11:27 +0200

    Run code checks

--- ureport/contacts/migrations/0028_alter_reporterscounter_index_together.py
+++ ureport/contacts/migrations/0028_alter_reporterscounter_index_together.py
@@ -6,12 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contacts', '0027_install_triggers'),
+        ("contacts", "0027_install_triggers"),
     ]
 
     operations = [
         migrations.AlterIndexTogether(
-            name='reporterscounter',
+            name="reporterscounter",
             index_together=set(),
         ),
     ]
