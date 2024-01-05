c50b3fed5e4a7d07bbb528900b43fef582dd4669
Norbert Kwizera <norkans7@gmail.com>
2024-01-05 15:20:40 +0200

    Add config for switching new brand

--- ureport/public/tests.py
+++ ureport/public/tests.py
@@ -54,7 +54,7 @@ class PublicTest(UreportTest):
         self.login(self.superuser)
         response = self.client.get(edit_url, SERVER_NAME="nigeria.ureport.io")
         self.assertTrue("form" in response.context)
-        self.assertEqual(len(response.context["form"].fields), 65)
+        self.assertEqual(len(response.context["form"].fields), 66)
 
     def test_count(self):
         count_url = reverse("public.count")
