538f85b59b344f8e26a1d751200823277a70b59f
Norbert Kwizera <norkans7@gmail.com>
2024-08-23 17:13:58 +0200

    Adjust old migrations to not use index_together

--- ureport/polls/migrations/0034_auto_20160323_1443.py
+++ ureport/polls/migrations/0034_auto_20160323_1443.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
     dependencies = [("polls", "0033_auto_20160322_0958")]
 
     operations = [
-        migrations.AlterIndexTogether(name="pollresultscounter", index_together=set([("org", "ruleset", "type")]))
+        # model pollresultscounter was removed, the time to adjust this migration to replace index_together
     ]
